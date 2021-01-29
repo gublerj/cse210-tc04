baf57327b856469af26dbc82519eaf953eec0cb1
gublerj <gub18002@byui.edu>
2021-01-29 14:30:52 -0700

    first draft keep_playing and higher_lower methods in the class player

--- hilo_template/hilo/game/player.py
+++ hilo_template/hilo/game/player.py
@@ -4,9 +4,17 @@ class player:
         """
         Asks the user if they want to keep playing and checks to see if they have 0 points.
         """
+        self.keep_playing = input("Do you want to keep playing? (y/n): ")
+        if self.points < 1:
+            return False
+        elif self.keep_playing == n:
+            return False
+        else:
+            return True
+
 
     def higher_lower(self):
         """
         Asks the user to guess if the next card will be higher or lower.
-        test comment
         """
+        self.guess = input("do you think the next card will be higher or lower? (h/l): ")
