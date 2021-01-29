936978e2bbbdfe84edd5e47892549edc8cca13c0
BrandonJPerry <per18061@byui.edu>
2021-01-29 13:01:46 -0700

    Structure for player and dealer class

---
+++ hilo_template/hilo/game/dealer.py
@@ -0,0 +1,17 @@
+import random
+
+class dealer:
+    """
+    Dealer generates a random number, sends it to the player.
+    Dealer determines whether the guess is a win or lose for that round.
+    """
+
+    def deal_card(self):
+        """
+        Generates random number between 1 and 13.
+        """
+    
+    def count_points(self):
+        """
+        Counts points and display total points.
+        """
